5b7258f94d3c225de888626681abb51199bea9d3
marksikaundi <emmanuelmark117@gmail.com>
2023-11-09 10:22:39 +0200

    Update profile image and header logo, reorder blog list

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -6,9 +6,9 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
         <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
+            <Image src={profileImg} alt="Otofast Gaming" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-        <span className="font-bold dark:font-semibold text-lg md:text-xl">Project</span>
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">Gaming</span>
     </Link>
   )
 }
