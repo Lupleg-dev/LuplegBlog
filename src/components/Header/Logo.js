d2118d70bdd92694b58fdb4716c941d465f43178
marksikaundi <emmanuelmark117@gmail.com>
2023-11-18 09:33:43 +0200

    Update Logo component

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,10 +5,10 @@ import profileImg from "@/public/profile-img.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 overflow-hidden border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="Otofast Gaming" className="w-full h-auto rounded-full" sizes="20vw" priority />
+        <div className=" w-12 md:w-16 overflow-hidden dark:border-gray  mr-2 md:mr-4">
+            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
         </div>
-        <span className="font-bold dark:font-semibold text-lg md:text-xl"></span>
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">Project</span>
     </Link>
   )
 }
