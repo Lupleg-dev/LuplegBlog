c397005cb4e47425cda8966a89e304f3471d6611
marksikaundi <emmanuelmark117@gmail.com>
2024-01-04 10:14:25 +0200

    Updated ptages

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -15,7 +15,7 @@ const RecentPosts = ({ blogs }) => {
           href="/categories/all"
           className="inline-block font-medium text-accent dark:text-accentDark underline underline-offset-2      text-base md:text-lg"
         >
-          view all
+          View All
         </Link>
       </div>
 
