1196b5bacbf1ffda277fe42ab5b9ae7c6cfedce3
marksikaundi <emmanuelmark117@gmail.com>
2024-01-04 09:29:02 +0200

    updated the heading

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,22 +1,23 @@
 // please update required information
 const siteMetadata = {
-    title: 'Otofast Project',
-    author: 'Mark Sikaundi',
-    headerTitle: 'Articles collection',
-    description: 'Proving world Technology trending stories and top Tech skills',
-    language: 'en-us',
-    theme: 'system', // system, dark or light
-    siteUrl: 'https://www.otofastproject.com', // your website URL
-    siteLogo: '/logo.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'mark@devcircleafrica.com', 
-    github: 'https://github.com/marksikaundi',
-    twitter: 'https://twitter.com/Alisikaundi',
-    facebook: 'https://facebook.com/marksikaundi',
-    youtube: 'https://youtube.com/devcircleafrica',
-    linkedin: 'https://www.linkedin.com/in/marksikaundi',
-    dribbble: 'https://www.dribbble.com/marksikaundi',
-    locale: 'en-US',
-  }
-  
-  module.exports = siteMetadata
+  title: "Otofast Project",
+  author: "Mark Sikaundi",
+  headerTitle: "Technology Trendings Around the Globe",
+  description:
+    "World classic technology stories distributor around the world from software engineering, gaming, cyber security, machine learning and many others.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://www.otofastproject.com", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "mark@devcircleafrica.com",
+  github: "https://github.com/marksikaundi",
+  twitter: "https://twitter.com/Alisikaundi",
+  facebook: "https://facebook.com/marksikaundi",
+  youtube: "https://youtube.com/devcircleafrica",
+  linkedin: "https://www.linkedin.com/in/marksikaundi",
+  dribbble: "https://www.dribbble.com/marksikaundi",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
